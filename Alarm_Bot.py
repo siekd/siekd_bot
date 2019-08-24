0a36e8d222583373d07bb18b6f7d9ba9e022155b
Chaoz <42206581+siekd@users.noreply.github.com>
2019-08-25 01:46:08 +0900

    Update Alarm_Bot.py

--- Alarm_Bot.py
+++ Alarm_Bot.py
@@ -1,4 +1,4 @@
-import os
+import os, time
 import telegram
 import urllib.request
 from bs4 import BeautifulSoup
@@ -10,33 +10,34 @@ chat_id = '814915551'
 
 Book_mark = ['명탐정 코난', '액터쥬(act-age)', '아무튼 귀여워']
 
-URL = 'https://manamoa12.net/bbs/board.php?bo_table=manga'
-req = urllib.request.Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
-html = urllib.request.urlopen(req)
-bs = BeautifulSoup(html.read(), 'html.parser')
+while True:
+    URL = 'https://manamoa12.net/bbs/board.php?bo_table=manga'
+    req = urllib.request.Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
+    html = urllib.request.urlopen(req)
+    bs = BeautifulSoup(html.read(), 'html.parser')
 
-nameList = bs.findAll('a', {'style': 'font-size:16px'})
-New_list = []
-Fix_list = []
-Upload_list = []
+    nameList = bs.findAll('a', {'style': 'font-size:16px'})
+    New_list = []
+    Fix_list = []
+    Upload_list = []
 
-for name in nameList:
+    for name in nameList:
     New_list.append(name.get_text())
-for i in New_list:
+    for i in New_list:
     remove = i.rfind('화')
     if remove == -1:
         remove = i.rfind('편')
     Fix_list.append(i[5:remove+1])
 
-f = open('Log.txt', 'r+', encoding='UTF8')
-save = f.readlines()
-revise = []
+    f = open('Log.txt', 'r+', encoding='UTF8')
+    save = f.readlines()
+    revise = []
 
-for i in range(len(save)):
-    temp = save[i].replace('\n', '')
-    revise.append(temp)
+    for i in range(len(save)):
+        temp = save[i].replace('\n', '')
+        revise.append(temp)
 
-if Fix_list != revise:
+    if Fix_list != revise:
     for i in range(len(Fix_list)):
         for j in range(len(Book_mark)):
             if Book_mark[j] in Fix_list[i]:
@@ -50,4 +51,4 @@ if Fix_list != revise:
     with open('Log.txt', 'w', encoding='UTF8') as file:
         file.writelines(Upload_list)
 
-f.close()
+    f.close()
